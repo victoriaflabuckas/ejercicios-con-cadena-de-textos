a61f6b11394e8df0b0084e42797d8b2f87004624
VictoriaLabuckas <victoriaflabuckas@gmail.com>
2021-06-09 15:31:01 -0300

    first commit

---
+++ script.js
@@ -0,0 +1,36 @@
+let texto = prompt("Ingrese un texto");
+console.log(texto.includes("a"));
+
+let texto1 = prompt("Ingrese un texto");
+console.log(texto1.toLocaleLowerCase());
+console.log(texto1.toUpperCase());
+
+let texto2 = prompt("Ingrese una palabra");
+console.log(texto2.charAt(0));
+
+let texto3 = prompt("Ingrese una frase");
+console.log(texto3.length);
+
+let numero = prompt("Ingrese un número");
+console.log(numero.length);
+
+let texto4 = prompt("Ingrese una palabra con 9 letras");
+console.log(texto4.charAt(8));
+
+let texto5 = "hola usted";
+console.log(texto5.replace(" ","*"));
+
+let texto6 = prompt("Escriba una letra");
+console.log(texto6.includes("e"));
+
+let texto7 = prompt("Escriba una cadena de texto");
+console.log(texto7.includes("e"));
+
+let texto8 = prompt("Escriba un texto");
+console.log(texto8.slice(1,9));
+
+let texto9 = prompt("Ingrese un nombre todo en minúscula");
+console.log(texto9.toLocaleUpperCase(0));
+
+
+
